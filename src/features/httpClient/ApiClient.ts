ae530c709ff6920c32815d77a4e7a1bc6999f7b2
ntthong1308 <nguyentrungthong789@gmail.com>
2026-03-31 09:38:55 +0700

    FE-04: update api login register.

--- src/features/httpClient/ApiClient.ts
+++ src/features/httpClient/ApiClient.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const apiClient = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_BASE_API_ENDPOINT,
+  baseURL: process.env.NEXT_PUBLIC_BASE_API_ENDPOINT || "http://localhost:8080/api/v1",
   headers: {
     "Content-Type": "application/json",
   },
