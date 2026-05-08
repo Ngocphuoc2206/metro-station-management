6e13184a0b30812fcee3ba55928f1b64c3549a52
Ngocphuoc2206 <tranngocphuoc.2000vta@gmail.com>
2026-05-08 21:01:38 +0700

    add generate QR code

--- src/main/java/com/backend/management_ticket_metro/service/S3_Storage/S3StorageService.java
+++ src/main/java/com/backend/management_ticket_metro/service/S3_Storage/S3StorageService.java
@@ -1,4 +1,71 @@
 package com.backend.management_ticket_metro.service.S3_Storage;
 
-public class S3StorageService {
+import com.backend.management_ticket_metro.common.ErrorCode;
+import com.backend.management_ticket_metro.exception.AppException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+import java.util.UUID;
+
+@Service
+@Slf4j
+public class S3StorageService implements StorageService {
+
+    @Autowired
+    private S3Client s3Client;
+
+    @Value("${storage.s3.bucket}")
+    private String bucket;
+
+    @Value("${storage.s3.public-base-url}")
+    private String publicBaseUrl;
+
+    @Override
+    public String uploadFile(MultipartFile file, String folder) {
+        try{
+            String extension = getExtension(file.getOriginalFilename());
+            String key = folder + "/" + UUID.randomUUID() + extension;
+
+            PutObjectRequest putObjectRequest = PutObjectRequest.builder()
+                    .bucket(bucket)
+                    .key(key)
+                    .contentType(file.getContentType())
+                    .build();
+
+            s3Client.putObject(
+                    putObjectRequest,
+                    RequestBody.fromBytes(file.getBytes())
+            );
+            return publicBaseUrl + "/" + key;
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            throw new AppException(ErrorCode.FILE_UPLOAD_FAILED);
+        }
+    }
+
+    @Override
+    public String uploadBytes(byte[] data, String fileName, String contentType, String folder) {
+        String extension = getExtension(fileName);
+        String key = folder + "/" + UUID.randomUUID() + extension;
+
+        PutObjectRequest putObjectRequest = PutObjectRequest.builder()
+                .bucket(bucket)
+                .key(key)
+                .contentType(contentType)
+                .build();
+
+        s3Client.putObject(putObjectRequest, RequestBody.fromBytes(data));
+        return publicBaseUrl + "/" + key;
+    }
+
+    private String getExtension(String fileName) {
+        if (fileName == null || !fileName.contains(".")) return "";
+        return fileName.substring(fileName.lastIndexOf("."));
+    }
 }
+
